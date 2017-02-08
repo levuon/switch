054e00ea611a77c721cce60810ac230a7472315f
liuhuan <levuon@163.com>
2017-02-08 11:27:36 +0800

    tmp listeners

--- src/switcher.js
+++ src/switcher.js
@@ -1,14 +1,15 @@
 
 
 function indexArray(arrs, target) {
-    var index = arrs.indexOf(target)
-    return arrs.splice(index, 1)
+    var index = tmpArry.indexOf(target)
+    return tmpArry.splice(index, 1)
 }
 
 export default function switcher() {
 
     var turnOnListeners = [],
-        turnOffListeners = [];
+        turnOffListeners = [],
+        tmpListener;
 
     const onTurnOn = (listener) => {
 
@@ -21,10 +22,12 @@ export default function switcher() {
     }
 
     const onTurnOff = (listener) => {
-        var offListener = indexArray(turnOnListeners, listener);
+        tmpListener = turnOnListeners.slice(0);
+        var offListener = indexArray(tmpListener, listener);
         if(offListener && offListener.length > 0) {
           turnOffListeners.push(offListener)
         }
+        tmpListener = null;
     }
 
     const trunOn = () => {
